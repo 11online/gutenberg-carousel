abe2b759b4c5b628cd4734a19842c03d312c99ed
Isaac Peifer <iwpeifer@gmail.com>
2018-03-28 10:39:20 -0600

    first commit

---
+++ plugin.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name: Carousel Block
+ * Plugin URI: https://11online.us/
+ * Description: Carousel Block for Gutenberg
+ * Author: Iwpeifer
+ * Author URI: https://11online.us/
+ * Version: 1.0.0
+ * License: GPL2+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * @package CGB
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Block Initializer.
+ */
+require_once plugin_dir_path( __FILE__ ) . 'src/init.php';
---
+++ src/block/block.js
@@ -0,0 +1,95 @@
+/**
+ * BLOCK: gutenberg-carousel
+ *
+ * Registering a basic block with Gutenberg.
+ * Simple block, renders and saves the same content without any interactivity.
+ */
+
+//  Import CSS.
+import './style.scss';
+import './editor.scss';
+
+const { __ } = wp.i18n; // Import __() from wp.i18n
+const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
+const TextControl = wp.components.TextControl;
+const BlockControls = wp.blocks.BlockControls;
+const AlignmentToolbar = wp.blocks.AlignmentToolbar;
+const Dropdown = wp.components.Dropdown;
+const PlainText = wp.blocks.PlainText;
+const RichText = wp.blocks.PlainText;
+const ColorPalette = wp.blocks.ColorPalette;
+const SelectControl = wp.components.SelectControl;
+/**
+ * Register: aa Gutenberg Block.
+ *
+ * Registers a new block provided a unique name and an object defining its
+ * behavior. Once registered, the block is made editor as an option to any
+ * editor interface where blocks are implemented.
+ *
+ * @link https://wordpress.org/gutenberg/handbook/block-api/
+ * @param  {string}   name     Block name.
+ * @param  {Object}   settings Block settings.
+ * @return {?WPBlock}          The block, if it has been successfully
+ *                             registered; otherwise `undefined`.
+ */
+
+let attributes = {
+	randomKey: {
+		type: 'string',
+		default: 'carousel'
+	}
+};
+
+registerBlockType( 'cgb/block-gutenberg-carousel', {
+	// Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
+	title: __( 'Carousel' ), // Block title.
+	icon: 'image-flip-horizontal', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
+	category: 'common', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
+	attributes: attributes,
+	keywords: [
+		__( 'Carousel' ),
+	],
+
+	/**
+	 * The edit function describes the structure of your block in the context of the editor.
+	 * This represents what the editor will render when the block is used.
+	 *
+	 * The "edit" property must be a valid function.
+	 *
+	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
+	 */
+	edit: function({ attributes, setAttributes, focus, setFocus, isSelected, className }) {
+
+		if(attributes.randomKey === 'carousel') {
+			const randomKey = "carousel" + Math.floor(Math.random() * 1000);
+			setAttributes({randomKey: randomKey});
+		}
+
+		const addRemoveRow = (
+			<div style={{textAlign: 'right'}}>
+				{ __("Add Panel:") }&nbsp;
+				<button type="button" style={{display: 'inline-block'}} className="components-button components-icon-button" onClick={() => {
+
+				}}><span className="dashicons dashicons-plus"></span></button>
+			</div>
+		);
+
+		return [
+
+		];
+	},
+
+	/**
+	 * The save function defines the way in which the different attributes should be combined
+	 * into the final markup, which is then serialized by Gutenberg into post_content.
+	 *
+	 * The "save" property must be specified and must be a valid function.
+	 *
+	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
+	 */
+	save: function({ attributes, className }) {
+		return (
+
+		);
+	},
+} );
