fe1e9c971cae8a50b452e8aa588c9c084cf9dcbb
Eric Debelak <ericdebelak@gmail.com>
2018-04-13 16:00:25 -0600

    removed plugin header

--- plugin.php
+++ plugin.php
@@ -1,17 +1,4 @@
 <?php
-/**
- * Plugin Name: Carousel Block
- * Plugin URI: https://11online.us/
- * Description: Carousel Block for Gutenberg
- * Author: Iwpeifer
- * Author URI: https://11online.us/
- * Version: 1.0.0
- * License: GPL2+
- * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
- *
- * @package CGB
- */
-
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
